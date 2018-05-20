f379de3a04d82d50a18ba4ba7e2457ba0dd001c1
Guruprakash43 <guruprakash43@gmail.com>
2018-05-20 21:58:01 +0530

    Complete Project

---
+++ CatchMe/app/src/main/java/com/wordpress/ongolex/catchme/result.java
@@ -0,0 +1,53 @@
+package com.wordpress.ongolex.catchme;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //code that displays the content in full screen mode
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
+        setContentView(R.layout.activity_result);
+
+        TextView scoreLabel = (TextView) findViewById(R.id.scoreLabel);
+        TextView highScoreLabel = (TextView) findViewById(R.id.highScoreLabel);
+
+        int score = getIntent().getIntExtra("SCORE", 0);
+        scoreLabel.setText(score + "");
+
+        SharedPreferences settings = getSharedPreferences("HIGH_SCORE", Context.MODE_PRIVATE);
+        int highScore = settings.getInt("HIGH_SCORE", 0);
+
+        if (score > highScore) {
+            highScoreLabel.setText("High Score : " + score);
+
+            // Update High Score
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("HIGH_SCORE", score);
+            editor.commit();
+
+        } else {
+            highScoreLabel.setText("High Score : " + highScore);
+
+        }
+
+    }
+
+
+    public void tryAgain(View view) {
+        startActivity(new Intent(getApplicationContext(), start.class));
+    }
+}
---
+++ CatchMe/app/src/main/java/com/wordpress/ongolex/catchme/start.java
@@ -0,0 +1,39 @@
+package com.wordpress.ongolex.catchme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //code that displays the content in full screen mode
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
+        setContentView(R.layout.activity_start);
+    }
+    public void startGame(View view) {
+        startActivity(new Intent(getApplicationContext(), main.class));
+    }
+
+    // Disable Return Button
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (event.getKeyCode()) {
+                case KeyEvent.KEYCODE_BACK:
+                    return true;
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
+    }
+}
